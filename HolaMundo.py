9c1923e8c034f5ae2452f75316aa1526ce9fadde
Fernando <ferrivas96@gmail.com>
2025-10-22 23:21:28 -0300

    Primera versión del README

---
+++ HolaMundo.py
@@ -0,0 +1,11 @@
+def hola_mundo() -> str:
+    """
+    Devuelve el saludo 'Hola mundo'.
+    """
+    return "Hola fernando"
+
+
+if __name__ == "__main__":
+    # Implementación/uso de la función
+    mensaje = hola_mundo()
+    print(mensaje)         # Muestra: Hola mundo
